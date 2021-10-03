6fe6143cebe2958b3deddf1312bde254e8172a9f
HIMlaoS_Misa <62408957+MisaLiu@users.noreply.github.com>
2021-10-04 01:43:04 +0800

    💡加好友私信适当增加延迟

--- request-event.js
+++ request-event.js
@@ -5,7 +5,9 @@ const { _settings, bot } = require('./index');
 bot.on('request.friend.add', function(e) {
 	bot.setFriendAddRequest(e.flag, true);
 	bot.sendGroupMsg(_settings.groupId, '有新用户加入了频道：' + e.user_id);
-	bot.sendPrivateMsg(e.user_id, '你好呀，欢迎加入本频道！\n请先阅读 https://www.kancloud.cn/himlaos_misa/midrai_channel_docs/2484124 以获取必要的信息');
+	setTimeout(function() {
+		bot.sendPrivateMsg(e.user_id, '你好呀，欢迎加入本频道！\n请先阅读 https://www.kancloud.cn/himlaos_misa/midrai_channel_docs/2484124 以获取必要的信息');
+	}, 4000);
 });
 
 // 处理邀请进群事件，默认不通过
